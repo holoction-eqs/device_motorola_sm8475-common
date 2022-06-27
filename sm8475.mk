151b57edd009fb14b6427eca86022da9c7be5309
Fenglin Wu <quic_fenglinw@quicinc.com>
2022-06-27 23:12:24 +0800

    sm8475-common: healthd-ext: Switch to AIDL health HAL implementation
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- sm8475.mk
+++ sm8475.mk
@@ -187,9 +187,10 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery \
+    android.hardware.health@1.0.vendor \
+    android.hardware.health@2.1.vendor
 
 # HIDL
 PRODUCT_PACKAGES += \
