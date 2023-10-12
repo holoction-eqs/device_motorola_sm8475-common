a3fcb596223abe0a272a8f95ba922077f805f560
DillerOFire <niktofe1@gmail.com>
2023-10-12 19:43:45 +1000

    sm8475-common: Add missing targets

--- sm8475.mk
+++ sm8475.mk
@@ -281,7 +281,6 @@ PRODUCT_PACKAGES += \
 
 # Keymint
 PRODUCT_PACKAGES += \
-    android.hardware.security.keymint-V1-ndk.vendor \
     android.hardware.security.secureclock-V1-ndk.vendor \
     android.hardware.security.sharedsecret-V1-ndk.vendor \
 
@@ -334,7 +333,8 @@ PRODUCT_PACKAGES += \
 
 # Perf
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.2.vendor \
+    vendor.qti.hardware.perf@2.3.vendor
 
 # Permissions
 PRODUCT_COPY_FILES += \
