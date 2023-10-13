86b6f9830139e55948ec1c681a03e0c7790ab4b7
DillerOFire <niktofe1@gmail.com>
2023-10-13 21:56:55 +1000

    sm8450-common: Underp missing targets and dup manifest

--- sm8475.mk
+++ sm8475.mk
@@ -103,6 +103,7 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libsndcardparser \
+    libtinycompress \
     vendor.qti.hardware.AGMIPC@1.0-service \
     vendor.qti.hardware.pal@1.0-impl
 
@@ -386,7 +387,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service
 
 # Protobuf
 PRODUCT_COPY_FILES += \
