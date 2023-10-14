2bbfbd14bf76e71c9c79343cfa78d17ab5f3efb4
DillerOFire <niktofe1@gmail.com>
2023-10-14 16:49:33 +1000

    sm8475-common: Switch hwc to vulkan and set renderer to skiavk

--- sm8475.mk
+++ sm8475.mk
@@ -171,6 +171,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
+TARGET_USES_VULKAN := true
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
