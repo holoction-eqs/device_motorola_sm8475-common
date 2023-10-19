2829d8665d0ee88701fc311b0d0f24ed14dba82b
DillerOFire <niktofe1@gmail.com>
2023-10-19 11:14:08 +1000

    sm8475-common: Preopt SystemUIGoogle

--- sm8475.mk
+++ sm8475.mk
@@ -403,6 +403,9 @@ PRODUCT_PACKAGES += \
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle
 
+# Preopt SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
