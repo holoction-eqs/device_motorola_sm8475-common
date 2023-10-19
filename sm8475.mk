c8f7add6a6964a9bf9d5869cafe56f68fcb89e22
DillerOFire <niktofe1@gmail.com>
2023-10-19 11:14:08 +1000

    sm8475-common: Preopt SystemUIGoogle

--- sm8475.mk
+++ sm8475.mk
@@ -385,6 +385,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+# Preopt SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
